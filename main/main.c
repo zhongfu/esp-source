d2f478431570f7a540b907c26789c695bd984ccd
Zhongfu Li <me@zhongfu.li>
2021-03-21 16:49:44 +0800

    fix use of deprecated esp vfs function

--- main/main.c
+++ main/main.c
@@ -22,7 +22,7 @@ static TaskHandle_t s_sling_task_handle;
 void app_main(void) {
   uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0);
   esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
-  esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_LF);
+  esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_LF);
   setvbuf(stdin, NULL, _IONBF, 0);
   setvbuf(stdout, NULL, _IONBF, 0);
 
